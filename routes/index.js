38985f52deac4eb46cdd720949b6c11778098aaf
이현구 <kibisama9@gamil.com>
2025-12-09 13:20:13 -0800

    0.0.1

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const auth = require("./auth");
 const user = require("./user");
+const api = require("./api");
 
 router.use("/auth", auth);
 router.use("/user", user);
+router.use("/api", api);
 
 module.exports = router;
